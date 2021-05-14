51ca313a9b9e14f16f014c028aebd95d87d8cb57
jéssica senger <jessicasbogoni1@gmail.com>
2021-05-14 17:46:11 -0400

    First commit

--- src/views/pages/home/Home.js
+++ src/views/pages/home/Home.js
@@ -1,17 +1,42 @@
+import { api } from '../../../service/api';
+
+const getJokesRandom = async () => {
+  const request = await api.get('random');
+  return request.data;
+};
+
+const getJokesCategory = async () => {
+  const request = await api.get('categories');
+  return request.data;
+};
+
 let Home = {
   is_private: false,
 
   render: async () => {
-      let view = `
+    const jokes = await getJokesRandom();
+    const categories = await getJokesCategory();
+    console.log(jokes);
+
+    let view = `
           <div>
-            <h1>Home</div>
+           <ul class="menu-list">
+            ${categories
+              .map(
+                (category, index) =>
+                  `<li class="menu-item" key=${index}>${category}</li>`
+              )
+              .join('')}
+           </ul>
+            <img src=${jokes.icon_url}>
+            <h1>${jokes.value}</div>
           </div>
       `;
 
-      return view
+    return view;
   },
 
-  after_render: async () => {}
-}
+  after_render: async () => {},
+};
 
 export default Home;
